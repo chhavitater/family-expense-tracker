28dbed8fdd9c0351f26fb90c795ec04685c34850
Chhavi Tater <chhavitater05@gmail.com>
2026-08-01 21:07:53 +0530

    Implement in-memory expenses REST API

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,26 @@
 import express from 'express';
 const app = express();
+app.use(express.json());
+
+let expenses = [];
+
+app.get('/expenses', (req, res) => {
+  res.status(200).json(expenses);
+});
+
+app.post('/expenses', (req,res) => {
+  const { description, amount} = req.body;
+  const id = Date.now().toString();
+  const newExpense = {id, description, amount};
+  expenses.push(newExpense);
+  res.status(201).json(newExpense);
+});
+
+app.delete('/expenses/:id', (req, res) => {
+  const id = req.params.id;
+  expenses = expenses.filter(expense => expense.id !== id);
+  res.status(204).send();
+})
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');

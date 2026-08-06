412126ff2764eeb64f6d3864b9c68883a0e078e0
Chhavi Tater <chhavitater05@gmail.com>
2026-08-06 10:00:15 +0530

    Migrate expenses API from in-memory storage to Prisma + PostgreSQL

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,46 @@
 import express from 'express';
+import { PrismaClient } from './generated/prisma/index.js';
+const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 
-let expenses = [];
-
-app.get('/expenses', (req, res) => {
-  res.status(200).json(expenses);
+app.get('/expenses', async (req, res) => {
+  try{
+    const expenses = await prisma.expense.findMany();
+    res.status(200).json(expenses);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Failed to fetch expenses'});
+  }  
 });
 
-app.post('/expenses', (req,res) => {
-  const { description, amount} = req.body;
-  const id = Date.now().toString();
-  const newExpense = {id, description, amount};
-  expenses.push(newExpense);
-  res.status(201).json(newExpense);
+app.post('/expenses', async (req, res) => {
+  try{
+    const { description, amount} = req.body;
+    
+    const newExpense = await prisma.expense.create({
+      data: { description, amount, paidByUserId: 'test-user-1'},
+    });
+    res.status(201).json(newExpense);
+  } catch (error){
+    console.error(error);
+    res.status(500).json({error: 'Failed to create expense'});
+  }
+  
 });
 
-app.delete('/expenses/:id', (req, res) => {
-  const id = req.params.id;
-  expenses = expenses.filter(expense => expense.id !== id);
-  res.status(204).send();
+app.delete('/expenses/:id', async (req, res) => {
+  try{
+    const id = req.params.id;
+    await prisma.expense.delete({
+      where: { id },
+    });
+    res.status(204).send();
+  }catch (error){
+    console.error(error);
+    res.status(500).json({error: 'Failed to delete expense'});
+  }
+  
 })
 
 app.get('/', (req, res) => {
